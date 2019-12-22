9366a9445bc9a21e91d9c95f759d1161451a7038
ferealqq <pekka.mattinen@outlook.com>
2019-12-22 22:56:25 +0200

    retrieving channels working

--- server/database/channel_data_resources.js
+++ server/database/channel_data_resources.js
@@ -4,6 +4,8 @@ function insertChannel(port,channelName){
 	return knex.insert({port: port, channel: channelName}).into("channels");
 }
 
+
 module.exports = {
 	insertChannel,
+	getChannels: () => knex.select("*").from('channels'),
 } 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,15 @@
+const express = require('express');
 const _ = require('lodash');
 const { OpenSocket } = require('./socket_handling/socket_handler');
+const channelRouter = require('./routers/channel_router');
 
-let socket = new OpenSocket(555);
+let socket = new OpenSocket(123);
 
+const app = express();
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.use('/channels',channelRouter);
+
+app.listen(4000);
