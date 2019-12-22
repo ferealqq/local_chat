2b59541b38a08a2c5a50e1c5fbd6548b8431a874
ferealqq <pekka.mattinen@outlook.com>
2019-12-22 23:58:22 +0200

    adding new channel is working

--- server/routers/ChannelRouter.js
+++ server/routers/ChannelRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const { getChannels } = require('../database/channel_data_resources.js');
-const 
+const { getChannels,insertChannel } = require('../database/channel_data_resources.js');
+const portfinder = require('portfinder');
+const _ = require('lodash');
 
 function ChannelRouter(socketServer){
 	this.socketServer = socketServer;
@@ -19,7 +20,23 @@ function ChannelRouter(socketServer){
 }
 
 ChannelRouter.prototype.handleNewChannelPost = function(req,res){
-	
+	let { channel } = req.body;
+	if(!_.isNil(channel)){
+		portfinder.getPort((err,port)=>{
+			if(!err){
+				insertChannel(port,channel).then((response)=>{
+					if(_.size(response) === 1 ){
+						this.socketServer.openNewSocket(port,channel);
+						res.json({msg: "New channel is now open!"});
+					}else{
+						res.status(500).json({msg: "Something went wrong while saving the new channel!"});
+					}
+				}).catch((err)=>{
+					res.status(500).json({msg: "Something went wrong! Channel by this name probably exists already!"});
+				});
+			}
+		});
+	}
 }
 
 ChannelRouter.prototype.getRouter = function(){
